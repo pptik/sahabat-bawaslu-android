58eb839ba9ca388a89472f0fd907d56656299464
emilhamep <emilhamep@gmail.com>
2018-02-27 16:51:39 +0700

    Inteface login dan struktur persiapan fungsi login

--- app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/login/LoginActivity.java
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/login/LoginActivity.java
@@ -1,9 +1,13 @@
 package id.pptik.ilham.sahabatbawaslu.features.login;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import id.pptik.ilham.sahabatbawaslu.R;
+import id.pptik.ilham.sahabatbawaslu.features.ui.CircleView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +15,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        CircleView circleView = new CircleView(this);
     }
+
+
 }
---
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/ui/CircleView.java
@@ -0,0 +1,57 @@
+package id.pptik.ilham.sahabatbawaslu.features.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import id.pptik.ilham.sahabatbawaslu.R;
+
+import static android.graphics.Color.*;
+
+/**
+ * Created by Ilham on 27/02/18.
+ */
+
+public class CircleView extends View{
+    public CircleView(Context context) {
+        super(context);
+
+        init(null);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet set){
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setShadowLayer(12, 0, 0, Color.DKGRAY);
+        paint.setShader(new LinearGradient(0, getHeight()*40/100, 0, getHeight(), Color.parseColor("#FD7E16"), Color.parseColor("#FAD961"), Shader.TileMode.MIRROR));
+        canvas.drawCircle(getWidth()/2,0,getWidth()*70/100,paint);
+        setLayerType(LAYER_TYPE_SOFTWARE, paint);
+
+    }
+}
