0439b5599dbbe1d7a3c9e3ab24a69c68314dd730
emilhamep <emilhamep@gmail.com>
2018-02-28 10:27:01 +0700

    MVVM pattern Implemented

--- app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/login/LoginActivity.java
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/login/LoginActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import id.pptik.ilham.sahabatbawaslu.R;
+import id.pptik.ilham.sahabatbawaslu.commands.UserInterface;
 import id.pptik.ilham.sahabatbawaslu.databinding.ActivityLoginBinding;
 import id.pptik.ilham.sahabatbawaslu.features.ui.CircleView;
 import id.pptik.ilham.sahabatbawaslu.models.UserModel;
@@ -25,8 +27,15 @@ public class LoginActivity extends AppCompatActivity {
         activityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
 
         UserViewModel userViewModel = new UserViewModel(new UserModel());
-        activityLoginBinding.setLogin(userViewModel);
-
+        //Binding View Model
+        activityLoginBinding.setUser(userViewModel);
+        //Binding Interface
+        activityLoginBinding.setLoginevent(new UserInterface() {
+            @Override
+            public void onClickLogin() {
+                Toast.makeText(LoginActivity.this, "Haloooooo", Toast.LENGTH_LONG).show();
+            }
+        });
 
         CircleView circleView = new CircleView(this);
 
