a87567b20d4af35f96bc89754aa3c2eeddf1506f
emilhamep <emilhamep@gmail.com>
2018-03-07 09:30:16 +0700

    Persiapan halaman awal ketika sdh masuk

--- app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/dashboard/DashboardActivity.java
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/dashboard/DashboardActivity.java
@@ -1,8 +1,11 @@
 package id.pptik.ilham.sahabatbawaslu.features.dashboard;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,10 +20,15 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         ButterKnife.bind(this);
+
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorAccent));
+
         toolbar.setTitle(getResources().getString(R.string.dashboard_label));
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
     @Override
