a4596ac61385c4a714fdbe1011c8a7ee85dcfee1
ilhamfa <emilhamep@gmail.com>
2018-03-20 19:04:38 +0700

    Tambah activity Tambah Forum

---
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/forum/AddForumActivity.java
@@ -0,0 +1,13 @@
+package id.pptik.ilham.sahabatbawaslu.features.forum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddForumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_forum);
+    }
+}
