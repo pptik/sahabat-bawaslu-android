364d110f5530f8f8687b38ab4ecfb0957ebb51f1
ilhamfa <emilhamep@gmail.com>
2018-03-21 08:01:32 +0700

    Implementasi Activity Detail Materi

---
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/learning/MaterialDetailActivity.java
@@ -0,0 +1,50 @@
+package id.pptik.ilham.sahabatbawaslu.features.learning;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import id.pptik.ilham.sahabatbawaslu.R;
+
+public class MaterialDetailActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_detail);
+
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.oranyeGelap));
+
+        ButterKnife.bind(this);
+        toolbar.setTitle(getResources().getString(R.string.materi_label));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}
