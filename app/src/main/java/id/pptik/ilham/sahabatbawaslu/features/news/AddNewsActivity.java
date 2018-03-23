1a1d499cc89d6d4302b9a22696c055b8579d972a
ilhamfa <emilhamep@gmail.com>
2018-03-23 15:48:30 +0700

    Tambah activity add news

---
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/news/AddNewsActivity.java
@@ -0,0 +1,15 @@
+package id.pptik.ilham.sahabatbawaslu.features.news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.pptik.ilham.sahabatbawaslu.R;
+
+public class AddNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_news);
+    }
+}
