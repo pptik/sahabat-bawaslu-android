8d92d9b00569effa89b2130fe9d7e74c794f0b76
ilhamfa <emilhamep@gmail.com>
2018-03-28 17:32:24 +0700

    Binding View Model to Activity News

--- app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/news/AddNewsActivity.java
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/news/AddNewsActivity.java
@@ -1,5 +1,6 @@
 package id.pptik.ilham.sahabatbawaslu.features.news;
 
+import android.databinding.DataBindingUtil;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,23 +9,40 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import id.pptik.ilham.sahabatbawaslu.R;
+import id.pptik.ilham.sahabatbawaslu.commands.NewsInterface;
+import id.pptik.ilham.sahabatbawaslu.databinding.ActivityAddNewsBinding;
+import id.pptik.ilham.sahabatbawaslu.databinding.ActivityLoginBinding;
+import id.pptik.ilham.sahabatbawaslu.models.NewsModel;
+import id.pptik.ilham.sahabatbawaslu.models.UserModel;
+import id.pptik.ilham.sahabatbawaslu.view_models.NewsViewModel;
+import id.pptik.ilham.sahabatbawaslu.view_models.UserViewModel;
 
 public class AddNewsActivity extends AppCompatActivity {
+    private ActivityAddNewsBinding activityAddNewsBinding;
     @BindView(R.id.toolbar)
     Button buttonSubmit;
     EditText editTextCaption;
-
     android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_news);
+        activityAddNewsBinding = DataBindingUtil.setContentView(this,R.layout.activity_add_news);
+        final NewsViewModel userViewModel = new NewsViewModel(new NewsModel());
+        //Binding View Model
+        activityAddNewsBinding.setNews(userViewModel);
+        activityAddNewsBinding.setAddnewsevent(new NewsInterface() {
+            @Override
+            public void onClickAddNews() {
+                Toast.makeText(AddNewsActivity.this, "Binded!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         editTextCaption = (EditText)findViewById(R.id.edit_text_caption);
         buttonSubmit = (Button)findViewById(R.id.button_masuk);
