364a7c0c6ad7b2a7c2d0189b44802fde4ea336d9
positiveneutron <emilhamep@gmail.com>
2018-04-23 11:53:07 +0700

    Automatic add hashtag on layout already implemented. SWAG

--- app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/forum/AddForumActivity.java
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/features/forum/AddForumActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,7 +18,10 @@ import id.pptik.ilham.sahabatbawaslu.R;
 
 public class AddForumActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)Toolbar toolbar;
+    @BindView(R.id.linearLayoutFlexibleEditText)LinearLayout linearLayoutFlexibleEditText;
+    @BindView(R.id.imageButtonAddHashtag)ImageButton imageButtonAddHashTag;
 
+    private int hashTagCounter = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +38,20 @@ public class AddForumActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-
+        imageButtonAddHashTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addHashtagEditText(hashTagCounter);
+                hashTagCounter++;
+            }
+        });
     }
 
+    private void addHashtagEditText(int hashTagCounter){
+        EditText editTextHashtag = new EditText(this);
+        editTextHashtag.setId(hashTagCounter);
+        linearLayoutFlexibleEditText.addView(editTextHashtag);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home){
