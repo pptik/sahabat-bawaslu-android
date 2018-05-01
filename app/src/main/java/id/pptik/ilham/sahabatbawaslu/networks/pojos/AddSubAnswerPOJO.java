5bf380389ae1f690a11674b72dfb88c351c5287a
positiveneutron <emilhamep@gmail.com>
2018-05-01 21:39:00 +0700

    Add POJO AddSubAnswer

---
+++ app/src/main/java/id/pptik/ilham/sahabatbawaslu/networks/pojos/AddSubAnswerPOJO.java
@@ -0,0 +1,210 @@
+package id.pptik.ilham.sahabatbawaslu.networks.pojos;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Ilham on 01/05/18.
+ */
+
+public class AddSubAnswerPOJO {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("rc")
+    @Expose
+    private String rc;
+    @SerializedName("rm")
+    @Expose
+    private String rm;
+    @SerializedName("results")
+    @Expose
+    private Results results;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getRc() {
+        return rc;
+    }
+
+    public void setRc(String rc) {
+        this.rc = rc;
+    }
+
+    public String getRm() {
+        return rm;
+    }
+
+    public void setRm(String rm) {
+        this.rm = rm;
+    }
+
+    public Results getResults() {
+        return results;
+    }
+
+    public void setResults(Results results) {
+        this.results = results;
+    }
+
+    public class Results {
+
+        @SerializedName("comment")
+        @Expose
+        private String comment;
+        @SerializedName("comment_id")
+        @Expose
+        private String commentId;
+        @SerializedName("level")
+        @Expose
+        private Integer level;
+        @SerializedName("post_by")
+        @Expose
+        private PostBy postBy;
+        @SerializedName("status")
+        @Expose
+        private Integer status;
+        @SerializedName("upvote")
+        @Expose
+        private Integer upvote;
+        @SerializedName("downvote")
+        @Expose
+        private Integer downvote;
+        @SerializedName("favorite")
+        @Expose
+        private Integer favorite;
+        @SerializedName("created_at")
+        @Expose
+        private String createdAt;
+        @SerializedName("updated_at")
+        @Expose
+        private List<Object> updatedAt = null;
+        @SerializedName("_id")
+        @Expose
+        private String id;
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public String getCommentId() {
+            return commentId;
+        }
+
+        public void setCommentId(String commentId) {
+            this.commentId = commentId;
+        }
+
+        public Integer getLevel() {
+            return level;
+        }
+
+        public void setLevel(Integer level) {
+            this.level = level;
+        }
+
+        public PostBy getPostBy() {
+            return postBy;
+        }
+
+        public void setPostBy(PostBy postBy) {
+            this.postBy = postBy;
+        }
+
+        public Integer getStatus() {
+            return status;
+        }
+
+        public void setStatus(Integer status) {
+            this.status = status;
+        }
+
+        public Integer getUpvote() {
+            return upvote;
+        }
+
+        public void setUpvote(Integer upvote) {
+            this.upvote = upvote;
+        }
+
+        public Integer getDownvote() {
+            return downvote;
+        }
+
+        public void setDownvote(Integer downvote) {
+            this.downvote = downvote;
+        }
+
+        public Integer getFavorite() {
+            return favorite;
+        }
+
+        public void setFavorite(Integer favorite) {
+            this.favorite = favorite;
+        }
+
+        public String getCreatedAt() {
+            return createdAt;
+        }
+
+        public void setCreatedAt(String createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public List<Object> getUpdatedAt() {
+            return updatedAt;
+        }
+
+        public void setUpdatedAt(List<Object> updatedAt) {
+            this.updatedAt = updatedAt;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public class PostBy {
+
+            @SerializedName("user_id")
+            @Expose
+            private String userId;
+            @SerializedName("username")
+            @Expose
+            private String username;
+
+            public String getUserId() {
+                return userId;
+            }
+
+            public void setUserId(String userId) {
+                this.userId = userId;
+            }
+
+            public String getUsername() {
+                return username;
+            }
+
+            public void setUsername(String username) {
+                this.username = username;
+            }
+
+        }
+
+    }
+}
